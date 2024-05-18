cc52fd0b149678999e06c0e477f05d9ef1ccb422
PaoloPci <paolopci@yahoo.it>
2024-05-18 16:40:37 +0200

    11. NG17-Control Flow Statements (@switch) step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,25 @@ import { Component, signal } from '@angular/core';
 export class AppComponent {
   title = 'myHoldApp17';
 
-  hasText: boolean = false;
+  employeeRole: string = '';
 
 
-  textInput(e: any) {  
-    (e.target.value).length > 3 ? this.hasText = true : this.hasText = false
-  }
+  employees = signal<Employee[]>([
+    { id: 1, name: 'EmployeeA', role: 'Admin' },
+    { id: 2, name: 'EmployeeB', role: 'HR' },
+    { id: 3, name: 'EmployeeC', role: 'Marketing' },
+    { id: 4, name: 'EmployeeD', role: 'Developer' },
+    { id: 5, name: 'EmployeeE', role: 'Marketing' },
+    { id: 6, name: 'EmployeeF', role: 'Analyst' },
+    { id: 7, name: 'EmployeeG', role: 'Developer' },
+    { id: 8, name: 'EmployeeH', role: 'Analyst' },
+    { id: 8, name: 'EmployeeI', role: 'HR' },
+  ]);
+}
+
+
+export interface Employee {
+  id: number;
+  name: string;
+  role: string;
 }
