bd6c74924388c2595f6790f006b907762d78465d
PaoloPci <paolopci@yahoo.it>
2024-05-20 22:04:53 +0200

    example 2 bis, come leggere previsionValue, currentValue e firstChange della class SimpleChange

--- src/app/Cap04/test/test.component.ts
+++ src/app/Cap04/test/test.component.ts
@@ -8,10 +8,31 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 export class TestComponent implements OnChanges {
 
   valore: string = '';
+  previsionValue: any;
+  currentValue: any;
+  isFirstChange: any;
+
+
+
+
   @Input() inputValue: string = '';
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Calling fron onChanges hook ...');
     console.log(changes);
-  }
 
+
+    // come leggere currentValue, previsionValue e firstChange dalla class SimpleChanges
+    // Fare un ciclo for come sotto
+    for (let propName in changes) {
+      let chng = changes[propName];
+      this.currentValue = JSON.stringify(chng.currentValue);
+      this.previsionValue = JSON.stringify(chng.previousValue);
+      this.isFirstChange = JSON.stringify(chng.firstChange)
+    }
+
+    
+    console.log('current value: ' + this.currentValue);
+    console.log('prevision value: ' + this.previsionValue);
+    console.log('is First change: ' + this.isFirstChange);
+  }
 }
