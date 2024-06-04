2fece68594799ada7908581f4cc7ae516059f554
PaoloPci <paolopci@yahoo.it>
2024-06-04 17:17:07 +0200

    example 07 - Multi Step Reactive Form add FormGroup,Validators,FormBuilder (Part 2/3)

--- src/app/Cap012/multi-step-reactive-form/multi-step-reactive-form.component.ts
+++ src/app/Cap012/multi-step-reactive-form/multi-step-reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-multi-step-reactive-form',
@@ -7,16 +8,39 @@ import { Component } from '@angular/core';
 })
 export class MultiStepReactiveFormComponent {
   step: number = 1;
+  myForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      userDetails: this.formBuilder.group({
+        fname: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      }),
+      additionalDetails: this.formBuilder.group({
+        mobile: ['', [Validators.required, Validators.maxLength(10)]],
+        address: ['', Validators.required],
+        country: ['', Validators.required],
+        gender: ['', Validators.required]
+      }),
+      feedback: this.formBuilder.group({
+        comments: [''],
+      })
+    });
+  }
 
 
 
   btnPrevious() {
     this.step--;
-   }
+  }
 
   btnNext() {
     this.step++;
-   }
+  }
+
+  formSubmit(){
+    console.log(this.myForm.value);
+  }
 
 }
